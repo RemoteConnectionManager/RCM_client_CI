61883563ea45983bf190fedc437fc289202eab88
Luigi Calori <luigi.calori@gmail.com>
2018-08-30 10:47:40 +0000

    rewrite just artifact platform, turbovnc extarnal bundle is the same... hopefully

--- scripts/build/linux/ubuntu_16_04_x64/setup.sh
+++ scripts/build/linux/ubuntu_16_04_x64/setup.sh
@@ -2,5 +2,5 @@
 
 export BUILD_USER=vagrant
 export BUILD_HOST=192.168.0.36
-export BUILD_PLATFORM=linux/64bit
-export BUILD_EXT_PATH=turbovnc_bundle/$BUILD_PLATFORM
+export BUILD_PLATFORM=ubuntu16/64bit
+export BUILD_EXT_PATH=turbovnc_bundle/linux/64bit
--- scripts/build/linux/ubuntu_18_04_x64/setup.sh
+++ scripts/build/linux/ubuntu_18_04_x64/setup.sh
@@ -2,5 +2,5 @@
 
 export BUILD_USER=vagrant
 export BUILD_HOST=192.168.0.38
-export BUILD_PLATFORM=linux/ubuntu18
-export BUILD_EXT_PATH=turbovnc_bundle/$BUILD_PLATFORM
+export BUILD_PLATFORM=ubuntu18/64bit
+export BUILD_EXT_PATH=turbovnc_bundle/linux/64bit
