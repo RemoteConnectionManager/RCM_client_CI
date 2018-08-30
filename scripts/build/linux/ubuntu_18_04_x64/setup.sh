8f819bf38d7f2d52fafc0109c29f5cdda6b52ac3
Luigi Calori <luigi.calori@gmail.com>
2018-08-30 10:24:09 +0000

    change artifact path for ubuntu18

--- scripts/build/linux/ubuntu_18_04_x64/setup.sh
+++ scripts/build/linux/ubuntu_18_04_x64/setup.sh
@@ -2,5 +2,5 @@
 
 export BUILD_USER=vagrant
 export BUILD_HOST=192.168.0.38
-export BUILD_PLATFORM=linux/64bit
+export BUILD_PLATFORM=linux/ubuntu18
 export BUILD_EXT_PATH=turbovnc_bundle/$BUILD_PLATFORM
