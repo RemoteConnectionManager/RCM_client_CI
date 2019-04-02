913a0f742d1d646fde44d5450b4942fcfbf65947
Luigi Calori <luigi.calori@gmail.com>
2019-04-02 17:09:53 +0200

    print error status of update but do not stop on error

--- scripts/build/linux/centos_7_x64/setup.sh
+++ scripts/build/linux/centos_7_x64/setup.sh
@@ -4,4 +4,4 @@ export BUILD_USER=centos
 export BUILD_HOST=192.168.0.77
 export BUILD_PLATFORM=centos7/64bit
 export BUILD_EXT_PATH=turbovnc_bundle/linux/64bit
-export BUILD_SUDO_SETUP="sudo yum check-update -t  && sudo yum install -y python-devel python-virtualenv  make automake gcc gcc-c++ kernel-devel"
+export BUILD_SUDO_SETUP="sudo yum check-update; echo returned status $? && sudo yum install -y python-devel python-virtualenv  make automake gcc gcc-c++ kernel-devel"
