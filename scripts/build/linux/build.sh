5819d21f1e0b9346f23614487ace1bd77f649bbb
Luigi Calori <luigi.calori@gmail.com>
2019-05-07 20:11:36 +0200

    now builds have folder hierarchi, move all

--- scripts/build/linux/build.sh
+++ scripts/build/linux/build.sh
@@ -73,5 +73,5 @@ echo "build result -->$build<--"
 
 ################## copy artifacts from vm #######
 mkdir -p $BUILD_RCM_ARTIFACTS_PATH/$BUILD_PLATFORM
-scp -r $BUILD_USER@${BUILD_HOST}:deploy/dist $BUILD_RCM_ARTIFACTS_PATH/$BUILD_PLATFORM/$BUILD_RCM_RELEASE
+scp -r $BUILD_USER@${BUILD_HOST}:deploy/dist/* $BUILD_RCM_ARTIFACTS_PATH
 
