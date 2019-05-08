296c793a7e394c1e9ff405779127ade03a4d18bb
Luigi Calori <luigi.calori@gmail.com>
2019-05-08 17:27:55 +0200

    first cut of darwin

---
+++ scripts/build/darwin/macmini/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export BUILD_USER=rcm
+export BUILD_HOST=localhost
+export BUILD_HOST_PORT=-p 22000
+export BUILD_PLATFORM=darwin/64bit
+export BUILD_EXT_PATH=no_turbovnc_folder
+export BUILD_SUDO_SETUP="echo NO_MACOS_INSTALL"
+export BUILD_VIRTUALENV_COMMAND="virtualenv-3.6"
