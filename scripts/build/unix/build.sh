008a401bf24daa84f09eb11ad5752f17a548d56d
Luigi Calori <luigi.calori@gmail.com>
2019-05-09 12:01:05 +0200

    refactored build script to be configured with darwin osx macmini setup

--- scripts/build/unix/build.sh
+++ scripts/build/unix/build.sh
@@ -19,20 +19,23 @@ export BUILD_RCM_RELEASE=$(cd $BUILD_RCM_SOURCE_PATH; git describe --tags --long
 export BUILD_RCM_EXTERNAL_PATH="$BUILD_ROOT_PATH/external" 
 export BUILD_RCM_ARTIFACTS_PATH="$BUILD_ROOT_PATH/artifacts" 
 
+
+###############   source specific VM setup ###########
+export BUILD_HOST_PORT=""
+source $CURR_PATH/$1/setup.sh
+export BUILD_SSH_COMMAND="ssh $BUILD_USER@${BUILD_HOST} ${BUILD_HOST_PORT}"
 ########################################################
 echo "BUILD_ROOT_PATH-->$BUILD_ROOT_PATH<--"
 echo "BUILD_RCM_SOURCE_PATH-->$BUILD_RCM_SOURCE_PATH<--"
 echo "BUILD_RCM_RELEASE-->$BUILD_RCM_RELEASE<--"
 echo "BUILD_RCM_EXTERNAL_PATH-->$BUILD_RCM_EXTERNAL_PATH<--"
 echo "BUILD_RCM_ARTIFACTS_PATH-->$BUILD_RCM_ARTIFACTS_PATH<--"
-###############   source specific VM setup ###########
-source $CURR_PATH/$1/setup.sh
 
 ############# setup #################################
-ssh $BUILD_USER@${BUILD_HOST} ${BUILD_SUDO_SETUP}
+${BUILD_SSH_COMMAND} ${BUILD_SUDO_SETUP}
 
 ##############   inspect VM environment ##############
-version=$(ssh $BUILD_USER@${BUILD_HOST} "python --version"  2>&1)
+version=$(${BUILD_SSH_COMMAND} "python --version"  2>&1)
 echo "system python version $version"
 
 #pip not needed#version=$(ssh $BUILD_USER@${BUILD_HOST} "pip --version")
@@ -42,36 +45,36 @@ echo "system python version $version"
 #uncomment#echo "unconditionally install virualenv"
 #uncomment#ssh $BUILD_USER@${BUILD_HOST} "pip3 install virtualenv"
 
-version=$(ssh $BUILD_USER@${BUILD_HOST} "${BUILD_VIRTUALENV_COMMAND} --version")
+version=$(${BUILD_SSH_COMMAND} "${BUILD_VIRTUALENV_COMMAND} --version")
 echo "virtualenv version $version"
 
-ssh $BUILD_USER@${BUILD_HOST} "if [ ! -f py3env/bin/activate ]; then ${BUILD_VIRTUALENV_COMMAND} py3env; fi"
+${BUILD_SSH_COMMAND} "if [ ! -f py3env/bin/activate ]; then ${BUILD_VIRTUALENV_COMMAND} py3env; fi"
 
 ##############   inspect virtualenv environment ##############
-version=$(ssh $BUILD_USER@${BUILD_HOST} "source py3env/bin/activate && python --version"  2>&1)
+version=$(${BUILD_SSH_COMMAND} "source py3env/bin/activate && python --version"  2>&1)
 echo "virtualenv python version $version"
 
 echo "############ test paramiko import in virtualenv #####"
-ssh $BUILD_USER@${BUILD_HOST} "source py3env/bin/activate && python -c \"exec(\\\"try: import paramiko\\\nexcept:\\\\n print( 'missing paramiko')\\\")\""
+${BUILD_SSH_COMMAND} "source py3env/bin/activate && python -c \"exec(\\\"try: import paramiko\\\nexcept:\\\\n print( 'missing paramiko')\\\")\""
 
 echo "############ unconditionally clean sandbox folder from VM"
-ssh $BUILD_USER@${BUILD_HOST} "if [ ! -f deploy ]; then rm -fr deploy; mkdir deploy; fi"
+${BUILD_SSH_COMMAND} "if [ ! -f deploy ]; then rm -fr deploy; mkdir deploy; fi"
 
 echo "############ unconditionally copy RCM folder to VM"
-scp -r $BUILD_RCM_SOURCE_PATH $BUILD_USER@${BUILD_HOST}:deploy 
+scp -r ${BUILD_HOST_PORT} $BUILD_RCM_SOURCE_PATH $BUILD_USER@${BUILD_HOST}:deploy 
 
 echo "install RCM requirements"
-ssh $BUILD_USER@${BUILD_HOST} "source py3env/bin/activate && which pip3 && pip3 install -r deploy/RCM/rcm/client/requirements.txt && python -c \"exec(\\\"try: import paramiko\\\nexcept:\\\\n print( 'missing paramiko')\\\")\""
+${BUILD_SSH_COMMAND} "source py3env/bin/activate && which pip3 && pip3 install -r deploy/RCM/rcm/client/requirements.txt && python -c \"exec(\\\"try: import paramiko\\\nexcept:\\\\n print( 'missing paramiko')\\\")\""
 
 echo "copy external bundle"
-scp -r $BUILD_RCM_EXTERNAL_PATH/$BUILD_EXT_PATH $BUILD_USER@${BUILD_HOST}:deploy/RCM/rcm/client/external/turbovnc 
+scp -r ${BUILD_HOST_PORT} $BUILD_RCM_EXTERNAL_PATH/$BUILD_EXT_PATH $BUILD_USER@${BUILD_HOST}:deploy/RCM/rcm/client/external/turbovnc 
 
 ################## build release with pyinstaller #######
 echo "building  both onedir nd onefile at same time"
-build=$(ssh $BUILD_USER@${BUILD_HOST} "source py3env/bin/activate && cd deploy && pyinstaller RCM/rcm/client/rcm_client_qt.spec ${BUILD_RCM_RELEASE} ${BUILD_PLATFORM}")
+build=$(${BUILD_SSH_COMMAND} "source py3env/bin/activate && cd deploy && pyinstaller RCM/rcm/client/rcm_client_qt.spec ${BUILD_RCM_RELEASE} ${BUILD_PLATFORM}")
 echo "build result -->$build<--"
 
 ################## copy artifacts from vm #######
 mkdir -p $BUILD_RCM_ARTIFACTS_PATH/$BUILD_PLATFORM
-scp -r $BUILD_USER@${BUILD_HOST}:deploy/dist/* $BUILD_RCM_ARTIFACTS_PATH
+scp -r ${BUILD_HOST_PORT} $BUILD_USER@${BUILD_HOST}:deploy/dist/* $BUILD_RCM_ARTIFACTS_PATH
 
