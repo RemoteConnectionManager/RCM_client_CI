e3a906d3d346963eaf2afe822577b6dd5cee76ac
Luigi Calori <luigi.calori@gmail.com>
2019-05-09 14:28:01 +0200

    add inspection

--- scripts/build/unix/build.sh
+++ scripts/build/unix/build.sh
@@ -31,6 +31,14 @@ echo "BUILD_RCM_RELEASE-->$BUILD_RCM_RELEASE<--"
 echo "BUILD_RCM_EXTERNAL_PATH-->$BUILD_RCM_EXTERNAL_PATH<--"
 echo "BUILD_RCM_ARTIFACTS_PATH-->$BUILD_RCM_ARTIFACTS_PATH<--"
 
+############# system inspection #################################
+inspect=$(${BUILD_SSH_COMMAND} "hostname; -python -c 'import sys; print(sys.platform)'")
+echo "inspect: $inspect"
+
+############# environment #################################
+environment=$(${BUILD_SSH_COMMAND} "env")
+echo "environment: $environment"
+
 ############# setup #################################
 ${BUILD_SSH_COMMAND} ${BUILD_SUDO_SETUP}
 
