c509958f73b5d7fae88b26c27dbd8de783404475
Luigi Calori <luigi.calori@gmail.com>
2019-05-09 10:11:30 +0200

    fix macmini

--- scripts/build/darwin/macmini/setup.sh
+++ scripts/build/darwin/macmini/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+# this need reverse tunnel active: issue
+# ssh -R 22000:localhost:22 ubuntu@130.186.13.241
+# so on jienkins can issue:
+# ssh rcm@localhost -p 22000
 export BUILD_USER=rcm
 export BUILD_HOST=localhost
-export BUILD_HOST_PORT=-p 22000
+export BUILD_HOST_PORT="-p 22000"
 export BUILD_PLATFORM=darwin/64bit
 export BUILD_EXT_PATH=no_turbovnc_folder
 export BUILD_SUDO_SETUP="echo NO_MACOS_INSTALL"
