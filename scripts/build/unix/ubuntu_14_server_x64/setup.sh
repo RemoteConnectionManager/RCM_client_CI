5e853744aa18b851d52871140c8a5e73cbf7472b
Luigi Calori <luigi.calori@gmail.com>
2019-05-21 17:48:54 +0200

    fix virtualenv missing apt package

--- scripts/build/unix/ubuntu_14_server_x64/setup.sh
+++ scripts/build/unix/ubuntu_14_server_x64/setup.sh
@@ -4,5 +4,5 @@ export BUILD_USER=ubuntu
 export BUILD_HOST=192.168.0.79
 export BUILD_PLATFORM=ubuntu14/64bit
 export BUILD_EXT_PATH=turbovnc_bundle/linux/64bit
-export BUILD_SUDO_SETUP="sudo apt-get update && sudo apt-get install -y python-pip python-dev build-essential virtualenv"
-export BUILD_VIRTUALENV_COMMAND="virtualenv -p python3"
+export BUILD_SUDO_SETUP="sudo apt-get update && sudo apt-get install -y python-pip python-dev build-essential  python3-pip"
+export BUILD_VIRTUALENV_COMMAND="python3 -m venv"
