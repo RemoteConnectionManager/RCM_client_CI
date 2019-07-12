e92da2c49bac350860a7b789e64856a54e73b26d
Luigi Calori <l.calori@cineca>
2019-07-12 09:29:16 +0000

    use python istead ot windows os for dir

--- scripts/build/win32/build.sh
+++ scripts/build/win32/build.sh
@@ -73,9 +73,11 @@ build=$(ssh $BUILD_USER@${BUILD_HOST} "cmd /C py3env\\\\Scripts\\\\activate \& c
 echo "build result -->$build<--"
 
 ################## copy artifacts from vm #######
-artifact_platform_folder=$(ssh $BUILD_USER@${BUILD_HOST} "cmd /C dir deploy\\\\dist \& dir")
+artifact_platform_folder=$(ssh $BUILD_USER@${BUILD_HOST} "cmd /C python -c \"import os; print(os.listdir(os.path.join(os.curdir, 'deploy', 'dist'))[0])\"")
 echo "artifact_platform_folder -->$artifact_platform_folder<--"
-artifact_folder=$(ssh $BUILD_USER@${BUILD_HOST} "cmd /C dir deploy\\\\dist\\\\${artifact_platform_folder} \& dir")
+
+echo "artifact_platform_folder -->$artifact_platform_folder<--"
+artifact_folder=$(ssh $BUILD_USER@${BUILD_HOST} "cmd /C python -c \"import os; print(os.listdir(os.path.join(os.curdir, 'deploy', 'dist' '${artifact_platform_folder}'))[0])\"")
 echo "artifact_folder -->$artifact_folder<--"
 
 scp -r $BUILD_USER@${BUILD_HOST}:deploy/dist/* $BUILD_RCM_ARTIFACTS_PATH
