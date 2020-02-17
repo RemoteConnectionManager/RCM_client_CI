02eaa45903ce934a970235ef41089faa05e35631
Simone Bnà <simone.bna@cineca.it>
2020-02-17 11:39:46 +0100

    Update build/unix/ubuntu_16_04_x64/setup.sh

--- scripts/build/unix/ubuntu_16_04_x64/setup.sh
+++ scripts/build/unix/ubuntu_16_04_x64/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export BUILD_USER=vagrant
-export BUILD_HOST=192.168.0.66
+export BUILD_HOST=192.168.0.84
 export BUILD_PLATFORM=ubuntu16/64bit
 export BUILD_EXT_PATH=turbovnc_bundle/linux/64bit
 export BUILD_SUDO_SETUP="sudo apt-get update && sudo apt-get install -y python-pip python-dev build-essential virtualenv"
