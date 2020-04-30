8727a3cc64c8575ab202cf4bb16d1abe7403d3cc
Luigi Calori <l.calori@cineca.it>
2020-04-30 22:00:34 +0200

    update centos vm ip address

--- scripts/build/unix/centos_7_x64/setup.sh
+++ scripts/build/unix/centos_7_x64/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export BUILD_USER=centos
-export BUILD_HOST=192.168.0.77
+export BUILD_HOST=192.168.0.106
 export BUILD_PLATFORM=centos7/64bit
 export BUILD_EXT_PATH=turbovnc_bundle/linux/64bit
 export BUILD_SUDO_SETUP="sudo yum check-update; echo returned status $? && sudo yum install -y  epel-release python36 python36-virtualenv make automake gcc gcc-c++ kernel-devel"
