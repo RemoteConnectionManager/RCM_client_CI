535fb5768d2ba8df7c79a53bbbfa3218966bc5d7
Luigi Calori <luigi.calori@gmail.com>
2021-03-03 17:13:48 +0100

    add setup script for ubuntu and windows

---
+++ scripts/dev_setup.bat
@@ -0,0 +1,5 @@
+
+python -m venv py3env.win32
+py3env.win32\Scripts\activate
+python -m pip install --upgrade pip
+pip install -r RCM\ircm\client\requirements.txt
---
+++ scripts/dev_setup_mint_18.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+# resolve $SOURCE until the file is no longer a symli nk
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+# if $SOURCE was a relative symli nk, 
+# we need to resolve it relative to the path where the symlink file was located
+done
+RCM_CI_ROOTPATH="$( cd -P "$( dirname "$( dirname "$SOURCE" )" )" && pwd )"
+RCM_TURBOVNC_PREFIX=${RCM_CI_ROOTPATH}/external/turbovnc_bundle/linux/64bit
+RCM_VIRTUALENV=${RCM_CI_ROOTPATH}/py3env
+if [ ! -d "$RCM_VIRTUALENV" ]; then
+  #virtualenv -p python3 $RCM_VIRTUALENV
+  #python3 -m venv $RCM_VIRTUALENV
+  pyvenv $RCM_VIRTUALENV
+  source ${RCM_VIRTUALENV}/bin/activate
+  pip install --upgrade pip==20.3
+  pip install -r ${RCM_CI_ROOTPATH}/RCM/rcm/client/requirements.txt
+else
+  source ${RCM_VIRTUALENV}/bin/activate
+fi
+
+export PATH=${RCM_TURBOVNC_PREFIX}/bin:$PATH
+export JAVA_HOME=${RCM_TURBOVNC_PREFIX}
+export JDK_HOME=${RCM_TURBOVNC_PREFIX}
+export JRE_HOME=${RCM_TURBOVNC_PREFIX}/jre
+export CLASSPATH=${JAVA_HOME}/lib:${JRE_HOME}/lib
+#export LD_LIBRARY_PATH=${RCM_TURBOVNC_PREFIX}/bin:$LD_LIBRARY_PATH
+
+alias RCM='python ${RCM_CI_ROOTPATH}/RCM/rcm/client/rcm_client_qt.py'
